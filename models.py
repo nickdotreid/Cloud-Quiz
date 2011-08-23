6a40cb44bf32cb6a5673ac2f5f2d148536b387d2
Nick Reid <nickreid@nickreid.com>
2011-08-23 14:04:11 -0700

    added answer model and references

--- models.py
+++ models.py
@@ -1,8 +1,22 @@
 from cloud_quiz import db
 
+class Answer(db.Model):
+	id = db.Column(db.Integer,primary_key=True)
+	question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+	term_id = db.Column(db.Integer, db.ForeignKey('term.id'))
+	
+	def __init__(self,question,term):
+		self.question = question
+		self.term = term
+		
+	def __repr(self):
+		return '<Answer %r>' % self.id
+
 class Question(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
 	text = db.Column(db.String(255))
+	
+	answers = db.relationship('Answer',backref='question',lazy="dynamic")
 	
 	def __init__(self, text):
 		self.text = text
@@ -13,6 +27,8 @@ class Question(db.Model):
 class Term(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
 	text = db.Column(db.String(255))
+	
+	answers = db.relationship('Answer',backref='term',lazy="dynamic")
 	
 	def __init__(self, text):
 		self.text = text
