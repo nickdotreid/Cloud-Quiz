3cbe69680b6ec834483111b790a714830daa210b
Nick Reid <nickreid@nickreid.com>
2011-09-09 14:44:37 -0700

    added validation

--- static/js/page.js
+++ static/js/page.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-	
+	$("form#ask_question").validate({
+		rules:{
+			text: {
+				required: true,
+				maxlength: 500
+			}
+		}
+		});
+		
+	$("form input,form textarea").each(function(){
+		input = $(this);
+		if($("label[for="+input.attr('id')+"].limit").length>0){
+			input.data("limit",Number($("label[for="+input.attr('id')+"].limit .number:first").text()))
+			
+			input.keyup(function(){
+				input = $(this)
+				characters_left = input.data("limit") - input.val().length
+				$("label[for="+input.attr('id')+"].limit").html('<span class="number">'+characters_left+'</span> characters left');
+				if(characters_left<1){
+					$("label[for="+input.attr('id')+"].limit .number").addClass("error");
+				}
+			}).keyup()
+			
+		}
+	})
+
 })
