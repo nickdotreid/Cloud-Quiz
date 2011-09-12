ed47262f014352f43318c8c942bdc241fab9c9f0
Nick Reid <nickreid@nickreid.com>
2011-09-12 12:15:01 -0700

    added new logo

--- db_config.py
+++ db_config.py
@@ -1,5 +1,5 @@
-db_uri = 'sqlite:////Users/nickreid/Documents/sfhiv/201108-TagCloud/CloudQuiz/test.db'
+db_uri = 'sqlite:////Users/nickreid/Documents/sfhiv/JustAsk/website/test.db'
 secrect_key = 'shhh, secrect'
 
-campaign_monitor_api_key = 'api key'
-campaign_monitor_list_id = 'justasksf list key'
+campaign_monitor_api_key = 'd731abfec259799453d9941547be1a69'
+campaign_monitor_list_id = '35568d5627dd1c6573e37cb0bc2c69cc'
