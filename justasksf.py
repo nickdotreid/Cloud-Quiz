7b589c2335295a2d2dbe61cf1d3e53a6a042ae32
Nick Reid <nickreid@nickreid.com>
2011-09-27 08:46:50 -0700

    added thank you sms

--- justasksf.py
+++ justasksf.py
@@ -51,6 +51,7 @@ def twilio_connect():
 						answer.question = question
 						db.session.add(answer)
 					db.session.commit()
+					message = "Your words have been added to our cloud"
 	if number and message:
 		client = TwilioRestClient(twilio_account_sid, twilio_auth_token)
 		message = client.sms.messages.create(to=number,
