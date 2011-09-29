b7f809e5066db13b98e39a433a5ef876862a4013
Nick Reid <nickreid@nickreid.com>
2011-09-28 18:13:46 -0700

    created blit-like like jqcloud creation

--- static/js/cloud.js
+++ static/js/cloud.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	$(".graph").bind("get",function(event){
 		graph = $(this);
+		graph.trigger("clear");
+		clone = graph.clone().addClass("clone").appendTo(graph).css("left",(0-graph.width()+"px"));
+		graph.addClass("loading").addClass("jqcloud");
 		$.ajax({
 			type:"POST",
 			dataType:"json",
@@ -11,11 +14,18 @@ $(document).ready(function(){
 				height:graph.height()
 			},
 			success:function(data){
-				$(".graph").jQCloud(data['words'],{callback:function(){
-					// something to animage here?
+				$(".graph.clone").jQCloud(data['words'],{callback:function(){
+					// if any words above or below height, adjust display
+					
+					// add words from graph.clone to graph
+					$(".graph.clone span").each(function(){
+						$(this).clone().appendTo($(".graph:not[.clone]"));
+						// set words in graph so offscreen, then animate to correct position
+					});
+					$(".graph.clone").remove();
 				}});
 			}});
+	}).bind("clear",function(event){
+		$(this).removeClass("loading").removeClass("jqcloud").html("");
 	});
-
-	
 });
