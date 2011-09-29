8e955d0533c121a1dd9ab6df2abbb341bafc72db
Nick Reid <nickreid@nickreid.com>
2011-09-29 09:25:31 -0700

    got unload animation to work

--- static/js/cloud.js
+++ static/js/cloud.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
-	$(".graph:not[.jqcloud]").bind("get",function(event){
+	$(".graph").bind("get",function(event){
 		graph = $(this);
-		graph.trigger("clear");
-		clone = graph.clone().html("").removeClass("graph").addClass("cloud").appendTo(graph).css("left",(0-graph.width()+"px"));
+		if($(".cloud",graph).length > 0){
+			graph.trigger("clear");
+			return false;
+		}
+		graph.trigger("load");
+	}).bind("load",function(){
+		graph = $(this);
+		if($(".cloud",graph).length > 0){
+			return false;
+		}
+		clone = graph.clone().html("").height(graph.height()).removeClass("graph").addClass("cloud").appendTo(graph).css("left",(0-graph.width()+"px"));
 		graph.addClass("loading");
 		$.ajax({
 			type:"POST",
@@ -11,7 +20,7 @@ $(document).ready(function(){
 			data:{
 				'question':graph.data("question")
 			},success:function(data){
-				$(".graph .cloud:last").jQCloud(data['words'],{callback:function(){
+				$(".graph .cloud:not[.old]").jQCloud(data['words'],{callback:function(){
 					// if any words above move them lower
 					$(".graph .cloud span").each(function(){
 						top_offset = $(this).position().top;
@@ -25,7 +34,13 @@ $(document).ready(function(){
 				}});
 			}});
 	}).bind("clear",function(event){
-		$(".cloud.jqcloud").remove();
+		$(".cloud.jqcloud",$(this)).addClass("old").animate(
+			{'left':$(this).width()+"px"},
+			{'animate':300,
+			'complete':function(){
+				setTimeout('$(".graph").trigger("load")');
+				$(this).remove();
+			}});
 		$(this).removeClass("loading");
 	});
 });
