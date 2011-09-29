1aa43e36ee1e2e18a9cb8be8800286067fa69ee8
Nick Reid <nickreid@nickreid.com>
2011-09-28 19:01:51 -0700

    graph height responsive to cloud

--- static/js/cloud.js
+++ static/js/cloud.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(".graph").bind("get",function(event){
+	$(".graph").data("original_height",$(".graph").height()).bind("get",function(event){
 		graph = $(this);
 		graph.trigger("clear");
 		clone = graph.clone().addClass("clone").appendTo(graph).css("left",(0-graph.width()+"px"));
@@ -9,14 +9,27 @@ $(document).ready(function(){
 			dataType:"json",
 			url:"/words",
 			data:{
-				question:graph.data("question"),
-				width:graph.width(),
-				height:graph.height()
-			},
-			success:function(data){
+				'question':graph.data("question")
+			},success:function(data){
 				$(".graph.clone").jQCloud(data['words'],{callback:function(){
-					// if any words above or below height, adjust display
-					
+					// if any words above move them lower
+					$(".graph.clone span").each(function(){
+						top_offset = $(this).position().top;
+						if(top_offset < 0){
+							top_offset = top_offset*-1;
+							$(".graph.clone span").each(function(){ $(this).css('top',(top_offset+$(this).position().top)+"px")});
+						}
+					});
+					// adjust bottom lower so words are not cut off
+					max_bottom = 0;
+					spans = $(".graph.clone span")
+					for(var i=0;i<spans.length;i++){
+						bottom = $(spans[i]).height()+$(spans[i]).position().top;
+						if(bottom > max_bottom){
+							max_bottom = bottom;
+						}
+					}
+					$(".graph:not[.clone]").height(max_bottom);
 					// add words from graph.clone to graph
 					$(".graph.clone span").each(function(){
 						$(this).clone().appendTo($(".graph:not[.clone]"));
@@ -26,6 +39,7 @@ $(document).ready(function(){
 				}});
 			}});
 	}).bind("clear",function(event){
+		$(this).height($(this).data("original_height"));
 		$(this).removeClass("loading").removeClass("jqcloud").html("");
 	});
 });
